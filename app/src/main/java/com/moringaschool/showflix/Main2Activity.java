ac1c11782d5677ef228b9b4a24b4e326744b8a4b
j0hnton <afrikiddo@gmail.com>
2019-05-12 20:57:10 +0300

    Making the activities

--- app/src/main/java/com/moringaschool/showflix/Main2Activity.java
+++ app/src/main/java/com/moringaschool/showflix/Main2Activity.java
@@ -2,12 +2,49 @@ package com.moringaschool.showflix;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        getSupportActionBar().setTitle("Activity 2");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        button2=(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain3Activity;
+            }
+        });
+
+public void openMain3Activity(){
+    intent=
+        }
     }
 }
+
+
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain2Activity();
+            }
+        });
+    }
+    public void openMain2Activity(){
+        intent intent= (intent) new Intent(this, Main2Activity.class);
+        startActivity((Intent) intent) ;
+    }
+}
+
