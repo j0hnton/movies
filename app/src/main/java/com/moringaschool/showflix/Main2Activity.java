472ede3edd9d34b6edb2cc5faf74449e3af483bf
j0hnton <afrikiddo@gmail.com>
2019-05-12 22:31:32 +0300

    Functionality of intents

--- app/src/main/java/com/moringaschool/showflix/Main2Activity.java
+++ app/src/main/java/com/moringaschool/showflix/Main2Activity.java
@@ -1,30 +1,37 @@
 package com.moringaschool.showflix;
 
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class Main2Activity<intent> extends AppCompatActivity {
     private Button button2;
+    private EditText EditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        EditText = (EditText) findViewById(R.id.nameEditText);
         button2 = (Button) findViewById(R.id.button2);
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMain3Activity();
             }
+            public void openMain3Activity(){
+                String Edit = EditText.getText().toString();
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                intent.putExtra("Edit",Edit);
+                startActivity(intent);
+            }
         });
-    }
-    public void openMain3Activity(){
-        Toast.makeText(Main2Activity.this, "Saved Successfully", Toast.LENGTH_LONG).show();
-        intent intent= (intent) new Intent(this, Main3Activity.class);
-        startActivity((Intent) intent) ;
     }
 }
--- app/src/main/java/com/moringaschool/showflix/Main3Activity.java
+++ app/src/main/java/com/moringaschool/showflix/Main3Activity.java
@@ -1,15 +1,19 @@
 package com.moringaschool.showflix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
-
+private TextView nameTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-
+        Intent intent=getIntent();
+        String Edit=((Intent) intent).getStringExtra("Edit");
+nameTextView.setText("Username:"+Edit);
     }
 }
