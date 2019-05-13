d628c1843dedd34f25aa6ec2ecaef0f5ff7eca6e
j0hnton <afrikiddo@gmail.com>
2019-05-13 15:07:19 +0300

    winding up on the backend

--- app/src/main/java/com/moringaschool/showflix/MainActivity.java
+++ app/src/main/java/com/moringaschool/showflix/MainActivity.java
@@ -1,20 +1,27 @@
 package com.moringaschool.showflix;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class MainActivity<intent> extends AppCompatActivity {
-private Button button;
+    @BindView(R.id.button) Button button;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.button);
+        ButterKnife.bind(this);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
