0f294036138f309be206834009214c8fc120572f
j0hnton <afrikiddo@gmail.com>
2019-05-22 17:00:35 +0300

    Modifying

--- app/src/main/java/com/moringaschool/showflix/Constants.java
+++ app/src/main/java/com/moringaschool/showflix/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     public static final String MOVIE_TOKEN =BuildConfig.MOVIE_TOKEN;
     public static final String MOVIE_TOKEN_QUERY_PARAMETER = "api_key";
     public static final String MOVIE_SEARCH_BASE_URL ="https://api.themoviedb.org/3/search/movie";
+    public static final String MOVIE_POPULAR_BASE_URL="https://api.themoviedb.org/3/movie/popular";
     public static final String  MOVIE_SEARCH_QUERY_PARAMETER = "query";
 }
--- app/src/main/java/com/moringaschool/showflix/Main2Activity.java
+++ app/src/main/java/com/moringaschool/showflix/Main2Activity.java
@@ -6,36 +6,55 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 public class Main2Activity<intent> extends AppCompatActivity {
-//    private Button button2;
-//    private EditText EditText;
-//public  static final String EXTRA_TEXT="com.moringaschool.showflix.moringaschool.EXTRA_TEXT";
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main2);
-//
-//        button2 = (Button) findViewById(R.id.button2);
-//        button2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openMain3Activity();
-//            }
-//        });
-//    }
-//    public void openMain3Activity(){
-//
-// EditText editText=(EditText) findViewById(R.id.nameEditText1);
-// String search=editText.getText().toString();
-//
-//        Toast.makeText(Main2Activity.this, "Saved Successfully", Toast.LENGTH_LONG).show();
-//        Intent intent= (Intent) new Intent(this, Main3Activity.class);
-//
-//        intent.putExtra(EXTRA_TEXT,search);
-//        startActivity((Intent) intent) ;
-//    }
+    private Button button2;
+    private EditText EditText;
+public  static final String EXTRA_TEXT="com.moringaschool.showflix.moringaschool.EXTRA_TEXT";
+    @BindView(R.id.signup)
+    TextView mSign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ButterKnife.bind(this);
+
+
+
+        button2 = (Button) findViewById(R.id.button3);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMain3Activity();
+            }
+        });
+        mSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = (Intent) new Intent(Main2Activity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void openMain3Activity(){
+
+ EditText editText=(EditText) findViewById(R.id.nameEditText1);
+ String search=editText.getText().toString();
+
+        Toast.makeText(Main2Activity.this, "Saved Successfully", Toast.LENGTH_LONG).show();
+        Intent intent= (Intent) new Intent(this, Main3Activity.class);
+
+        intent.putExtra(EXTRA_TEXT,search);
+        startActivity((Intent) intent) ;
+    }
+
+
 }
